08c3355c06955ad11f1a18c2fd7d20d03e523b87
Angeneris Cifuentes <angenerisc@gmail.com>
2024-08-22 09:51:15 +0700

    commit added chatbot api routes and fetch data

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,43 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true, // Enable React Strict Mode
+    swcMinify: true, // Enable SWC-based minification for faster builds
+  
+    // Environment Variables
+    env: {
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+      NPS_API_KEY: process.env.NPS_API_KEY,
+      WEATHER_API_KEY:process.env.WEATHER_API_KEY
 
-export default nextConfig;
+    },
+  
+    // Custom Webpack Configuration
+    webpack(config) {
+      // Custom Webpack configuration goes here
+      return config;
+    },
+  
+    // Redirects
+    async redirects() {
+      return [
+        {
+          source: '/old-path',
+          destination: '/new-path',
+          permanent: true,
+        },
+      ];
+    },
+  
+    // Rewrites
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: '/api/your-backend/:path*',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
